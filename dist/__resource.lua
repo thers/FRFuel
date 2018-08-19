a35f1e5ddb2cc21967678312a95f00cb337fe2de
Vespura <31419184+TomGrobbe@users.noreply.github.com>
2018-08-19 20:46:18 +0200

    Moved gas stations into a json file for dynamic gas stations & pump locations.
    It's working, but it may need improvements before pushing to master. @thers please review.
    ref: #24

--- dist/__resource.lua
+++ dist/__resource.lua
@@ -1,4 +1,7 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
-
+files { 
+    "config.ini",
+    "Newtonsoft.Json.dll",
+    "GasStations.json"
+}
 client_script "frfuel.net.dll"
-file "config.ini"
